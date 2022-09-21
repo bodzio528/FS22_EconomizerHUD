930830843dd6ec234248485c34aaf53eecdda9ff
Bogumił Chojnowski <bogumil.chojnowski@gmail.com>
2022-09-21 19:28:24 +0200

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,8 @@
+rem make ZIP archive
+tar.exe -a -c -f FS22_EconomizerHUD.zip modDesc.xml modIcon.dds EconomizerHUD.lua
+
+rem copy ZIP to FS22 mods folder
+xcopy /b/v/y FS22_EconomizerHUD.zip "%userprofile%\Documents\My Games\FarmingSimulator2022\mods"
+
+rem make update mod as well
+copy /b/v/y FS22_EconomizerHUD.zip FS22_EconomizerHUD_update.zip
